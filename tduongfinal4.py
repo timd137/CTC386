1b9cc0ee1df8c14fd0d282c1f540dc8196a441a0
timd137 <tduong47@toromail.csudh.edu>
2026-01-29 01:37:04 +0000

    CTC 386 Final Exam Question 4

---
+++ tduongfinal4.py
@@ -0,0 +1,10 @@
+#Timothy Duong
+#CTC 386 Final Exam Question 4
+
+def happy_new_year(integer):
+    for i in range(integer):
+        print("Happy New Year!")
+
+number = int(input("Input a number between 1 and 10: "))
+
+happy_new_year(number)
